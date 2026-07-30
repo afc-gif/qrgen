5ca240db9ba504909401bb41dc073a4986dbd678
DjCeejay <lordceejay12@gmail.com>
2026-07-30 21:55:22 +0100

    Redirect GET generate route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\QrController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QrController::class, 'show'])->name('qr.show');
+Route::get('/generate', fn () => redirect()->route('qr.show'));
 Route::post('/generate', [QrController::class, 'generate'])->name('qr.generate');
 Route::get('/download', [QrController::class, 'download'])->name('qr.download');
 Route::get('/reset', [QrController::class, 'reset'])->name('qr.reset');
